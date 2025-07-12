58fa36fa3df75de616f93d7dc65b0da5dbdcf9a2
Vimal379 <vimalprajapat379@gmail.com>
2025-07-12 23:38:05 +0530

    Adding timestamp project code

---
+++ timestamp-project/index.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var app = express();
+
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api/:date?", function (req, res) {
+  const dateParams = req.params.date;
+  let date;
+
+  if (!dateParams) {
+    date = new Date();
+  } else {
+    date = /\d{13}/.test(dateParams)
+      ? new Date(parseInt(dateParams))
+      : new Date(dateParams);
+  }
+
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  return res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
